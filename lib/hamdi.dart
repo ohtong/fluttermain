6c93de3be1175d673c8f787870110d43c2eb2b07
ohtong <gsleep48@gmail.com>
2024-10-23 20:33:49 +0700

    commit pertama

---
+++ lib/hamdi.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/hamdi.dart';
+import 'package:pertemuan1/splash1.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Splash1(),
+    );
+  }
+}
---
+++ splash1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/hamdi.dart';
+s
+class Splash1 extends StatelessWidget {
+  const Splash1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
